92eb9f366969079e59cf8cb5a5180bc15d76550a
ychooni <starctak@gmail.com>
2017-07-22 19:59:08 +0900

    KOI Regional 2006 ES 2

---
+++ 2577.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int cnt[10];
+
+int main()
+{
+	int i, m;
+	int a, b, c;
+	scanf("%d%d%d", &a, &b, &c);
+
+	m = a*b*c;
+	while(m) {
+		cnt[m%10]++;
+		m /= 10;
+	}
+	for(i=0; i<10; i++)
+		printf("%d\n", cnt[i]);
+
+	return 0;
+}
