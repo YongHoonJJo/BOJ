6e79c033cb0bb720ed90e9f958e23a82de1e7777
ychooni <starctak@gmail.com>
2017-08-01 20:10:06 +0900

    KOI 1996 ES 2 // DFS with Cycle

---
+++ 2668.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int g[101];
+bool visited[101], finished[101];
+vector<int> ans;
+
+void dfs(int cur)
+{
+	visited[cur] = 1;
+
+	int next = g[cur];
+	if(cur == next) {
+		finished[cur] = 1;
+		ans.push_back(cur);
+		return ;
+	}
+	if(!visited[next]) dfs(next);
+	else {
+		if(!finished[next]) {
+			for(int j=next; j!=cur; j=g[j]) {
+				finished[j] = 1;
+				ans.push_back(j);
+			}
+			finished[cur] = 1;
+			ans.push_back(cur);
+		}
+	}
+	finished[cur] = 1;
+}
+
+int main()
+{
+	int n, k;
+	scanf("%d", &n);
+
+	for(int i=1; i<=n; i++) {
+		scanf("%d", &k);
+		g[i] = k;
+	}
+
+	for(int i=1; i<=n; i++)
+		if(!visited[i])
+			dfs(i);
+	
+	int sz = ans.size();
+	sort(ans.begin(), ans.end());
+	printf("%d\n", sz);
+	for(int i=0; i<sz; i++)
+		printf("%d\n", ans[i]);
+
+	return 0;
+}
