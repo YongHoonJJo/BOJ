54bef1bbe00360b7c270c450376b3eed2ec3ab1d
ychooni <starctak@gmail.com>
2017-08-09 21:45:01 +0900

    KOI Regional 2015 ES 4, HS 4 / pSum / codersbrunch

---
+++ 10800.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct P { int c, s, i; };
+
+bool cmp(P a, P b)
+{
+	return a.s < b.s;
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	vector<P> v;
+	vector<vector<int> > sz(2001);
+	for(int i=0; i<n; i++) {
+		int c, s;
+		scanf("%d%d", &c, &s);
+		v.push_back((P){c, s, i});
+	}
+
+	sort(v.begin(), v.end(), cmp);
+
+	int pSum = 0;
+	vector<int> ans(n), cSum(n+1, 0);
+	for(int i=0, j=0; i<n; i++) {
+		while(v[j].s < v[i].s) {
+			pSum += v[j].s;
+			cSum[v[j].c] += v[j].s;
+			j++;
+		}
+		ans[v[i].i] = pSum - cSum[v[i].c];
+	}
+
+	for(int i=0; i<n; i++)
+		printf("%d\n", ans[i]);
+
+	return 0;
+}
