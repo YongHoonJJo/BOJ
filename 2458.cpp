765112ca0f22a5c124c70fb21e218a9b167878b8
ychooni <starctak@gmail.com>
2017-08-18 10:50:53 +0900

    KOI Regional 2011 ES 4

--- 2458.cpp
+++ 2458.cpp
@@ -2,32 +2,18 @@
 #include <vector>
 using namespace std;
 
-vector<int> L[501], H[501];
 int ansL[501], ansH[501]; 
 int visitedL[501], visitedH[501]; 
 
-int dfsL(int cur, int k)
+int dfs(vector<int> *v, int *visited, int cur, int &k)
 {
 	int ret=0;
-	visitedL[cur] = k;
+	visited[cur] = k;
 
-	for(int i=0; i<L[cur].size(); i++) {
-		int next = L[cur][i];
-		if(visitedL[next] != k)
-			ret += dfsL(next, k)+1;
-	}
-	return ret;
-}
-
-int dfsH(int cur, int k)
-{
-	int ret=0;
-	visitedH[cur] = k;
-
-	for(int i=0; i<H[cur].size(); i++) {
-		int next = H[cur][i];
-		if(visitedH[next] != k)
-			ret += dfsH(next, k)+1;
+	for(int i=0; i<v[cur].size(); i++) {
+		int next = v[cur][i];
+		if(visited[next] != k)
+			ret += dfs(v, visited, next, k)+1;
 	}
 	return ret;
 }
@@ -37,6 +23,7 @@ int main()
 	int n, m;
 	scanf("%d%d", &n, &m);
 
+	vector<int> L[501], H[501];
 	while(m--) {
 		int a, b;
 		scanf("%d%d", &a, &b);
@@ -45,8 +32,8 @@ int main()
 	}
 
 	for(int i=1; i<=n; i++) {
-		ansL[i] = dfsL(i, i);	
-		ansH[i] = dfsH(i, i);
+		ansL[i] = dfs(L, visitedL, i, i);	
+		ansH[i] = dfs(H, visitedH, i, i);
 	}	
 
 	int ans=0;
